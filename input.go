4b0640aec8fbd22441165df846f2679ded06c61c
AJ <aj@laatu.uk>
2017-03-06 22:00:32 +0000

    refactor

--- input.go
+++ input.go
@@ -3,20 +3,21 @@ package main
 import (
 	"errors"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 func readInput() (input []byte, err error) {
 	if stdinStat, _ := os.Stdin.Stat(); (stdinStat.Mode() & os.ModeCharDevice) == 0 {
 		if input, err = ioutil.ReadAll(os.Stdin); err != nil {
-			perr(err)
+			log.Fatalln(err)
 		}
 	} else if len(os.Args) > 1 {
 		if input, err = ioutil.ReadFile(os.Args[1]); err != nil {
-			perr(err)
+			log.Fatalln(err)
 		}
 	} else {
-		return nil, errors.New("Missing input")
+		err = errors.New("Missing input")
 	}
 
 	return
--- yaml2json.go
+++ yaml2json.go
@@ -4,22 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-yaml/yaml"
+	"log"
 )
 
 func main() {
 	input, err := readInput()
 
 	if err != nil {
+		fmt.Println(err)
+		fmt.Println()
 		printHelp()
 	}
 
 	var content interface{}
 	if err = yaml.Unmarshal([]byte(input), &content); err != nil {
-		perr(err)
+		log.Fatalln(err)
 	}
 
 	if content, err := json.Marshal(decode(content)); err != nil {
-		perr(err)
+		log.Fatalln(err)
 	} else {
 		fmt.Println(string(content))
 	}
