6f100618166b8d12ea5f93aa1bf0169163d0ce87
AJ <aj@48k.io>
2018-06-25 08:39:32 +0100

    some CI systems are sending wrong signals ^^

--- input.go
+++ input.go
@@ -8,12 +8,13 @@ import (
 )
 
 func readInput() (input []byte, err error) {
-	if stdinStat, _ := os.Stdin.Stat(); (stdinStat.Mode() & os.ModeCharDevice) == 0 {
-		if input, err = ioutil.ReadAll(os.Stdin); err != nil {
+
+	if len(os.Args) > 1 {
+		if input, err = ioutil.ReadFile(os.Args[1]); err != nil {
 			log.Fatalln(err)
 		}
-	} else if len(os.Args) > 1 {
-		if input, err = ioutil.ReadFile(os.Args[1]); err != nil {
+	} else if stdinStat, _ := os.Stdin.Stat(); (stdinStat.Mode() & os.ModeCharDevice) == 0 {
+		if input, err = ioutil.ReadAll(os.Stdin); err != nil {
 			log.Fatalln(err)
 		}
 	} else {
